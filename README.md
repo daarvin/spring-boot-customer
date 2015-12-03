78a71f38345e0e8f9bce2169cca24f9b7f68ec1c
Aravind Rajamani <aravind.rajamani@gmail.com>
2015-12-03 09:04:49 -0700

    Updated service to take query params

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# spring-boot-customer
-Contains spring boot implementation patterns examples
+# Sample web service implementation using Spring Boot
+Build:
+	mvn clean package
+	
+Run: 
+   java -jar target/customer-rest-services-0.1.0.jar
--- src/main/java/customer/web/CustomerController.java
+++ src/main/java/customer/web/CustomerController.java
@@ -13,9 +13,12 @@ public class CustomerController {
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/customer")
-    public Customer customer() {
+    public Customer customer(
+    	@RequestParam(value="firstName", defaultValue="Aravind") String firstName,
+    	@RequestParam(value="lastName", defaultValue="Rajamani") String lastName) 
+    {
         return new Customer(counter.incrementAndGet(),
-                            "Aravind",
-                            "Rajamani");
+                            firstName,
+                            lastName);
     }
 }
